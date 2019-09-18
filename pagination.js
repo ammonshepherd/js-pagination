8adea55aa1412741499b7908d77d12458ac0f2a2
Ammon Shepherd <ammon.uva@gmail.com>
2019-09-18 13:51:01 -0400

    Make it all work
    - move all the javascript into one file.
    - use materialize CSS cause I suck at design

--- pagination.js
+++ pagination.js
@@ -1,3 +1,11 @@
+/* Constants */
+RESULTS_PER_PAGE = 10;
+
+SEARCH_RESULTS = 'searchResults' // ID of element to display search results
+PAGINATE = 'paginate' // ID of element to display pagination results
+
+
+
 /**
  * pagination()
  *
@@ -32,7 +40,10 @@
  * @throws {Error} (If any parameter is not a positive number.)
  *
  * modified from: https://www.freecodecamp.org/news/https-medium-com-gladchinda-hacks-for-creating-javascript-arrays-a1b80cb372b/
+ * author: Ammon Shepherd
+ * date: August 12, 2019
  */
+
 function pagination(total_results, start_number, results_page = 10) {
     // Test that the first 3 arguments are finite numbers.
     // Using Array.prototype.every() and Number.isFinite().
@@ -122,5 +133,112 @@ function pagination(total_results, start_number, results_page = 10) {
 
     return pages;
 }
+
+/**
+ * buildPagination()
+ *
+ * Generates and inserts the pagination links into the HTML document. This
+ * function should be called when the page is loaded. The function takes the
+ * total number of results returned from the search query.
+ *
+ * @param {number} results_total (Required: The total number of results
+ *    returned from the search query.)
+ *
+ * @throws {Error} (If any parameter is not a positive number.)
+ *
+ * author: Ammon Shepherd
+ * date: August 22, 2019
+ */
+
+function buildPagination(results_total) {
+    // Ensure the step is always a positive number.
+    if (results_total <= 0) {
+        throw new Error('total_results must be a number greater than 0.');
+    }
+
+    const url = new URL(window.location.origin + window.location.pathname);
+    let searchParams = new URLSearchParams(window.location.search);
+    var params = (new URL(document.location)).searchParams;
+    var current_start = parseInt(params.get('start')); // value of start parameter
+    // if the URL parameter 'start' is empty, change it to 0, the first page 
+    if (!current_start) {
+        current_start = 0;
+    }
+    // the start param returns the results to start on, to convert to the
+    // current page number, divide by results per page and add 1.
+    var cp = (current_start / RESULTS_PER_PAGE) + 1; // current page number
+
+    // If there are no results, just return some text saying that.
+    if (results_total < 1) {
+        const result = document.getElementById(SEARCH_RESULTS);
+        result.insertAdjacentHTML('afterbegin', '<h2>No results found.</h2>');
+        return;
+    }
+
+    // Generate an array of page numbers, using the pagination function
+    let pages_list = pagination(results_total, current_start, RESULTS_PER_PAGE);
+
+    const paginate = document.getElementById(PAGINATE); 
+
+    // loop through the array of pages, and construct a link for each page
+    // number. The current page number does not become a link. Also create the
+    // previous and next links.
+    for (n = 0; n < pages_list.length; n++) {
+        var page_num = pages_list[n]; // the page number from the array
+
+        // if the page number is actually an elipse, just return an elipse
+        // without a link
+        if (page_num == "...") {
+            paginate.insertAdjacentHTML('beforeend', `<span class="page-link">${page_num}</span>`);
+
+        } else {
+            // start = where the search results should start. 
+            // This is the page number (in the array), minus one (1), times the
+            // number of results per page
+            var start = (page_num - 1) * RESULTS_PER_PAGE; 
+
+            
+            // if the page number in the array that we are on is the same as
+            // the current page (calculated from the start parameter) then this
+            // is the current page, 
+            if (page_num == cp) {
+
+                // Show the previous link if the current page is greater than 5
+                if (cp > 5) {
+                    // the start value should be the results per page quantity
+                    // less than the current start value
+                    params.set('start', start - RESULTS_PER_PAGE);
+                    paginate.insertAdjacentHTML('afterbegin', `<span class="page-link"><a href='${url.toString()}?${params.toString()}'>&#9756;</a></span>`)
+                }
+
+                // Show the next link if the current page is 5 places less than
+                // the last one (which you get by getting the value of the last
+                // element in the pages_list array) then subtract 5
+                if (cp < pages_list[pages_list.length - 1] - 4 ) {
+                    // if the current page is 1, then start should be 0
+                    if (cp == 1){
+                        start = 0;
+                    }
+                    params.set('start', start + RESULTS_PER_PAGE);
+                    paginate.insertAdjacentHTML('afterend', `<div class="page-link next-link"><a href='${url}?${params.toString()}'>&#9758;</a></div>`)
+                }
+
+                // Show the current page number, without a link, then skip the
+                // rest of the code in the loop and go to the next element in
+                // the array
+                paginate.insertAdjacentHTML('beforeend', `<span class="page-link current-page-link btn-floating btn-large">${page_num}</span>`);
+                continue;
+            }
+
+            // display the rest of the links
+            params.set('start', start);
+            paginate.insertAdjacentHTML('beforeend', `<span class="page-link"><a href='${url}?${params.toString()}'>${page_num}</a></span>`);
+        }
+
+
+    }
+
+}
+
 
 
